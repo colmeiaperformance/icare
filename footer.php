b7f8850ee95a1e174501fe19591310cea38b21f9
Becky Marques <becky.marques@colmeiaperformance.com.br>
2021-09-24 00:54:16 -0300

    Transfer tag footer para arquivo footer.php

--- footer.php
+++ footer.php
@@ -1,3 +1,80 @@
+<footer id="ica-footer" class="ica-the the-highlight">
+    <div class="ica-wrapper">
+        <dl class="footer-item">
+            <dt>I Care Brasil</dt>
+            <h3>MAPA</h3>
+            <p>Belo Horizonte, Brasil <br>
+                Av. Getúlio Vargas, 1492, 2° andar Svassi
+            </p>
+        </dl>
+        <dl class="footer-item">
+            <dt>Produtos e Projetos</dt>
+            <dd><a href="#">Alinhamento 2 Graus</a></dd>
+            <dd><a href="#">Finanças Sustentáveis</a></dd>
+            <dd><a href="#">Estratégia e Biodiversidade</a></dd>
+            <dd><a href="#">Analise de Cicli de Vida</a></dd>
+            <dd><a href="#">Objetivos do Desenvolvimento Sustentável (ODS)</a></dd>
+            <dt><a href="#">Cadastre Seu Currículo</a></dt>
+        </dl>
+        <dl class="footer-item">
+            <dt>Fale Com a Gente</dt>
+            <dd>
+
+                <a href="#"> <img src="<?php echo get_template_directory_uri() . '/images/tel-icon.png' ?>"
+                        alt="tel-icon">+55 (11) 3515-6135</a>
+            </dd>
+            <dd>
+
+                <a href="#"><img src="<?php echo get_template_directory_uri() . '/images/email-icon.png' ?>"
+                        alt="email-icon">contato-brasil@i-care-consult.com</a>
+            </dd>
+            <div class="footer-icons">
+                <a href="#"><img src="<?php echo get_template_directory_uri() . '/images/linkedin-icon.png' ?>"
+                        alt="linkedin-icon"></a>
+                <a href="#"><img src="<?php echo get_template_directory_uri() . '/images/twitter-icon.png' ?>"
+                        alt="twitter-icon"></a>
+                <a href="#"><img src="<?php echo get_template_directory_uri() . '/images/instagram-icon.png' ?>"
+                        alt="instagram-icon"></a>
+                <a href="#"><img src="<?php echo get_template_directory_uri() . '/images/youtube-icon.png' ?>"
+                        alt="youtube-icon"></a>
+            </div>
+            <dt>Receba Nossa Newsletter</dt>
+            <div class="footer-form">
+                <form action="" method="post">
+                    <input type="text" name="newsletter" placeholder="e-mail" class="ica-input">
+                    <input type="submit" value="Enviar" class="ica-input">
+                </form>
+            </div>
+        </dl>
+    </div>
+    <div class="footer-nav">
+        <div class="nav-container">
+            <p>I Care Brasil 2022</p>
+            <div class="nav-itens">
+                <div class="nav-item">
+                    <a href="#">Créditos</a>
+                </div>
+                <div class="nav-item">
+                    <a href="#">Legal</a>
+                </div>
+                <div class="nav-item">
+                    <a href="#">Privacidade</a>
+                </div>
+                <div class="nav-item">
+                    <a href="#">Aviso de Privacidade</a>
+                </div>
+                <div class="nav-item">
+                    <a href="#">Política de Cookies</a>
+                </div>
+                <div class="nav-item">
+                    <a href="#">Preferências de Cookies</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+
 <?php wp_footer(); ?>
 </body>
+
 </html>
--- index.php
+++ index.php
@@ -233,119 +233,54 @@
         </div>
     </section>
     <section class="ica-sec sec-notice ica-the the-white" id="notice">
-				<div class="ica-wrapper">
-					<header class="sec-header">
-						<h3 class="sec-title ica-the the-white">Últimas notícias</h3>
-					</header>
-					<div class="sec-itens">
-						<div class="item-content">
-							<div class="item-media">
-								<img src="<?php echo get_template_directory_uri() . '/images/blog-media.jpg' ?>" alt="notice">
-								<p class="media-title"></p>
-							</div>
-							<div class="media-description">
-								<p>A I Care Brasil acaba de concluir um estudo, em parceria com o Observatório Nacional e a Universidade de São Paulo</p>
-							</div>
-							<div class="media-footer">
-								<a href="#" class="ica-but but-conversion">Leia Mais</a>
-							</div>
-						</div>
-						<div class="item-content">
-							<div class="item-media">
-								<img src="<?php echo get_template_directory_uri() . '/images/blog-media2.jpg' ?>" alt="notice">
-								<p class="media-title"></p>
-							</div>
-							<div class="media-description">
-								<p>A I Care Brasil acaba de concluir um estudo, em parceria com o Observatório Nacional e a Universidade de São Paulo</p>
-							</div>
-							<div class="media-footer">
-								<a href="#" class="ica-but but-conversion">Leia Mais</a>
-							</div>
-						</div>
-						<div class="item-content">
-							<div class="item-media">
-								<img src="<?php echo get_template_directory_uri() . '/images/blog-media3.jpg' ?>" alt="notice">
-								<p class="media-title"></p>
-							</div>
-							<div class="media-description">
-								<p>A I Care Brasil acaba de concluir um estudo, em parceria com o Observatório Nacional e a Universidade de São Paulo</p>
-							</div>
-							<div class="media-footer">
-								<a href="#" class="ica-but but-conversion">Leia Mais</a>
-							</div>
-						</div>
-					</div>
-				</div>
-			</section>
-</main>
-<footer id="ica-footer" class="ica-the the-highlight">
-			<div class="ica-wrapper">
-				<dl class="footer-item">
-					<dt>I Care Brasil</dt>
-					<h3>MAPA</h3>
-                    <p>Belo Horizonte, Brasil <br>
-                        Av. Getúlio Vargas, 1492, 2° andar Svassi
-                    </p>
-				</dl>
-				<dl class="footer-item">
-					<dt>Produtos e Projetos</dt>
-					<dd><a href="#">Alinhamento 2 Graus</a></dd>
-					<dd><a href="#">Finanças Sustentáveis</a></dd>
-					<dd><a href="#">Estratégia e Biodiversidade</a></dd>
-					<dd><a href="#">Analise de Cicli de Vida</a></dd>
-                    <dd><a href="#">Objetivos do Desenvolvimento Sustentável (ODS)</a></dd>
-                    <dt><a href="#">Cadastre Seu Currículo</a></dt>
-				</dl>
-				<dl class="footer-item">
-					<dt>Fale Com a Gente</dt>
-					<dd>
-                  
-                        <a href="#">  <img src="<?php echo get_template_directory_uri() . '/images/tel-icon.png' ?>" alt="tel-icon">+55 (11) 3515-6135</a>
-                    </dd>
-					<dd>
-                    
-                        <a href="#"><img src="<?php echo get_template_directory_uri() . '/images/email-icon.png' ?>" alt="email-icon">contato-brasil@i-care-consult.com</a>
-                    </dd>
-                    <div class="footer-icons" >
-                        <a href="#"><img src="<?php echo get_template_directory_uri() . '/images/linkedin-icon.png' ?>" alt="linkedin-icon"></a>
-                        <a href="#"><img src="<?php echo get_template_directory_uri() . '/images/twitter-icon.png' ?>" alt="twitter-icon"></a>
-                        <a href="#"><img src="<?php echo get_template_directory_uri() . '/images/instagram-icon.png' ?>" alt="instagram-icon"></a>
-                        <a href="#"><img src="<?php echo get_template_directory_uri() . '/images/youtube-icon.png' ?>" alt="youtube-icon"></a>     
+        <div class="ica-wrapper">
+            <header class="sec-header">
+                <h3 class="sec-title ica-the the-white">Últimas notícias</h3>
+            </header>
+            <div class="sec-itens">
+                <div class="item-content">
+                    <div class="item-media">
+                        <img src="<?php echo get_template_directory_uri() . '/images/blog-media.jpg' ?>" alt="notice">
+                        <p class="media-title"></p>
                     </div>
-                    <dt>Receba Nossa Newsletter</dt>
-                    <div class="footer-form">
-                        <form action="" method="post">
-                            <input type="text" name="newsletter" placeholder="e-mail" class="ica-input">
-                            <input type="submit" value="Enviar" class="ica-input">
-                        </form>
+                    <div class="media-description">
+                        <p>A I Care Brasil acaba de concluir um estudo, em parceria com o Observatório Nacional e a
+                            Universidade de São Paulo</p>
                     </div>
-				</dl>
-			</div>
-            <div class="footer-nav">
-                    <div class="nav-container">
-                        <p>I Care Brasil 2022</p>
-                        <div class="nav-itens">
-                            <div class="nav-item">
-                                <a href="#">Créditos</a>
-                            </div>
-                            <div class="nav-item">
-                                <a href="#">Legal</a>
-                            </div>
-                            <div class="nav-item">
-                                <a href="#">Privacidade</a>
-                            </div>
-                            <div class="nav-item">
-                                <a href="#">Aviso de Privacidade</a>
-                            </div>
-                            <div class="nav-item">
-                                <a href="#">Política de Cookies</a>
-                            </div>
-                            <div class="nav-item">
-                                <a href="#">Preferências de Cookies</a>
-                            </div>
-                        </div>
+                    <div class="media-footer">
+                        <a href="#" class="ica-but but-conversion">Leia Mais</a>
                     </div>
                 </div>
-		</footer>
+                <div class="item-content">
+                    <div class="item-media">
+                        <img src="<?php echo get_template_directory_uri() . '/images/blog-media2.jpg' ?>" alt="notice">
+                        <p class="media-title"></p>
+                    </div>
+                    <div class="media-description">
+                        <p>A I Care Brasil acaba de concluir um estudo, em parceria com o Observatório Nacional e a
+                            Universidade de São Paulo</p>
+                    </div>
+                    <div class="media-footer">
+                        <a href="#" class="ica-but but-conversion">Leia Mais</a>
+                    </div>
+                </div>
+                <div class="item-content">
+                    <div class="item-media">
+                        <img src="<?php echo get_template_directory_uri() . '/images/blog-media3.jpg' ?>" alt="notice">
+                        <p class="media-title"></p>
+                    </div>
+                    <div class="media-description">
+                        <p>A I Care Brasil acaba de concluir um estudo, em parceria com o Observatório Nacional e a
+                            Universidade de São Paulo</p>
+                    </div>
+                    <div class="media-footer">
+                        <a href="#" class="ica-but but-conversion">Leia Mais</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</main>
+
 
 <?php get_footer() ?>
