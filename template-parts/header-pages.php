8d1aff66eff0ef197ea81eda65b500305f0ef1bf
Igor Fonseca <50646655+igorstutz@users.noreply.github.com>
2021-09-30 15:24:38 -0300

    correção header-pages

--- template-parts/header-pages.php
+++ template-parts/header-pages.php
@@ -1,4 +1,4 @@
-<header>
+<header class="ica-sec sec-header">
     <div class="sec-media">
         <img src="<?php echo get_template_directory_uri() . '/images/banner-sobre.jpg' ?>" alt="" />
         <p class="ica-the the-contrast"><?php echo get_the_title(); ?></p>
