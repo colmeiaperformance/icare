ba4b0b9563c71bf4f9a770aa32c6901d501943e3
Igor Fonseca <50646655+igorstutz@users.noreply.github.com>
2021-09-30 15:54:12 -0300

    estrutura page-clima

---
+++ page-clima.php
@@ -0,0 +1,119 @@
+<?php get_header(); ?>
+
+<?php get_template_part('template-parts/header-pages'); ?>
+
+<main id="ica-content">
+    
+    <section class="ica-sec sec-infos" id="infos">
+        <div class="ica-wrapper">
+            <div class="sec-header">
+                <p class="ica-the the-gray-222">
+                    O clima do mundo está mudando. Parte dessa mudança é natural, mas o ritmo com que ela vem acontecendo representa um importante desafio para os sistemas humanos atuais. A mudança climática é a alteração de longo prazo nos padrões climáticos médios em todo o mundo. Desde meados do século XIX, os seres humanos contribuíram para a liberação de dióxido de carbono e outros gases de efeito estufa no ar, o que resultou em rápidas alterações nas temperaturas globais que causam efeitos de longo prazo no clima e impactos sobre os assentamentos humanos.<br><br>
+                    Desde a Revolução Industrial, a temperatura média do planeta aumentou cerca de 1°C. Essa é uma mudança rápida. Em razão disso, diversas cidades e países ao redor do mundo já decretaram emergência climática, uma vez que elas vêm observando aumento dos impactos causados por essas alterações. Esses impactos vão muito além do aumento da temperatura, afetando ecossistemas e comunidades. Coisas das quais dependemos e valorizamos – água, energia, transporte, vida selvagem, agricultura, ecossistemas e saúde humana – estão sofrendo esses impactos negativos, e isso afeta pessoas, cidades e negócios.
+                </p>
+            </div>
+            <div class="sec-content">
+                <div class="content-media">
+                <img src="<?php echo get_template_directory_uri() . '/images/content-clima.jpg' ?>"
+                            alt="clima">
+                </div>
+                <div class="content-description">
+                    <p>
+                        Em um esforço coletivo para frear a emissão de gases de efeito estufa e reduzir a instabilidade relacionada ao clima, centenas de países assinaram o Acordo de Paris, cujo objetivo é conter o aquecimento global a 2°C. Apesar dos grandes desafios, a mudança climática também representa uma oportunidade para os setores público e privado, que podem apoiar a transição para uma economia de baixo carbono.<br><br>
+                        Nossa oferta para acompanhar os setores públicos e privados na definição de uma estratégia para reduzir as emissões e conter os efeitos negativos da alteração climática é estruturada em diferentes abordagens:    
+                    </p>
+                </div>
+            </div>
+        </div>    
+    </section>
+    <section class="ica-sec sec-services" id="services">
+        <div class="ica-wrapper">
+            <div class="item-service">
+                <h4> </h4>
+            </div>
+            <div class="item-service">
+                <h4> </h4>
+            </div>
+            <div class="item-service">
+                <h4> </h4>
+            </div>
+            <div class="item-service">
+                <h4> </h4>
+            </div>
+            <div class="item-service">
+                <h4> </h4>
+            </div>
+            
+        </div>
+    </section>
+    <section class="ica-sec sec-whatwedo" id="whatwedo">
+        <div class="ica-wrapper">
+            <div class="sec-header">
+                <h3 class="sec-title ica-the the-green240 the-contrast">O que fazemos</h3>
+            </div>
+            <div class="sec-content">
+                <div class="sec-item ica-the the-highlight">
+                    
+                </div>
+                <div class="sec-item ica-the the-highlight">
+                    
+                </div>
+                <div class="sec-item ica-the the-highlight">
+                    
+                </div>
+                
+            </div>
+        </div>
+    </section>
+    <section class="ica-sec sec-notice ica-the the-white" id="notice">
+        <div class="ica-wrapper">
+            <header class="sec-header">
+                <h3 class="sec-title ica-the the-white">Últimas notícias</h3>
+            </header>
+            <div class="sec-itens">
+                <div class="item-content">
+                    <div class="item-media">
+                        <img src="<?php echo get_template_directory_uri() . '/images/blog-media.jpg' ?>" alt="notice">
+                        <p class="media-title"></p>
+                    </div>
+                    <div class="media-description">
+                        <p>A I Care Brasil acaba de concluir um estudo, em parceria com o Observatório Nacional e a
+                            Universidade de São Paulo</p>
+                    </div>
+                    <div class="media-footer">
+                        <a href="#" class="ica-but but-conversion">Leia Mais</a>
+                    </div>
+                </div>
+                <div class="item-content">
+                    <div class="item-media">
+                        <img src="<?php echo get_template_directory_uri() . '/images/blog-media2.jpg' ?>" alt="notice">
+                        <p class="media-title"></p>
+                    </div>
+                    <div class="media-description">
+                        <p>A I Care Brasil acaba de concluir um estudo, em parceria com o Observatório Nacional e a
+                            Universidade de São Paulo</p>
+                    </div>
+                    <div class="media-footer">
+                        <a href="#" class="ica-but but-conversion">Leia Mais</a>
+                    </div>
+                </div>
+                <div class="item-content">
+                    <div class="item-media">
+                        <img src="<?php echo get_template_directory_uri() . '/images/blog-media3.jpg' ?>" alt="notice">
+                        <p class="media-title"></p>
+                    </div>
+                    <div class="media-description">
+                        <p>A I Care Brasil acaba de concluir um estudo, em parceria com o Observatório Nacional e a
+                            Universidade de São Paulo</p>
+                    </div>
+                    <div class="media-footer">
+                        <a href="#" class="ica-but but-conversion">Leia Mais</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</main>
+
+
+<?php get_footer() ?>
