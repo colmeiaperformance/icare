1f552c9673f3fbd560a8add43f39565edb264c31
Becky Marques <becky.marques@colmeiaperformance.com.br>
2021-10-06 17:57:09 -0300

    Aside em template part

--- page-blog.php
+++ page-blog.php
@@ -101,39 +101,10 @@
 
                 </div>
             </div>
-            <aside class="aside-blog">
-                <div class="aside-search">
-                    <form action="" method="post" class="form-search">
-                        <input type="text" name="search" class="ica-input">
-                        <input type="submit" value="Buscar" class="ica-but but-conversion">
-                    </form>
-                </div>
+            
 
-                <dl class="footer-item">
-                    <dt>Mais Lidas</dt>
-                    <dd><a href="#">Lorem ipsum</a></dd>
-                    <dd><a href="#">Lorem ipsum</a></dd>
-                    <dd><a href="#">Lorem ipsum</a></dd>
-                    <dd><a href="#">Lorem ipsum</a></dd>
-                </dl>
-                <dl class="footer-item">
-                    <dt>Categorias</dt>
-                    <dd><a href="#">Biodiversidade</a></dd>
-                    <dd><a href="#">Clima</a></dd>
-                    <dd><a href="#">Finanças Sustentáveis</a></dd>
-                    <dd><a href="#">Escritório Virtual</a></dd>
-                </dl>
-                <dl class="footer-item">
-                    <dt>Assine nossa newsletter</dt>
-                    <form action="" class="form-news">
-                        <input type="text" placeholder="Nome" class="input-news">
-                        <input type="text" placeholder="E-mail" class="input-news">
-                        <input type="submit" value="Enviar" class="ica-but but-conversion">
-                    </form>
-                </dl>
+            <?php get_template_part( '/template-parts/aside-blog' ) ?>
 
-
-            </aside>
         </div>
     </section>
 
