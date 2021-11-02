0c82ca375779ce028ec1d991b2e888c9a4df81d6
Becky Marques <becky.marques@colmeiaperformance.com.br>
2021-11-02 01:14:10 -0300

    Conteúdo flexível page.php

---
+++ page.php
@@ -0,0 +1,9 @@
+<?php get_header(); ?>
+<?php get_template_part('template-parts/header-pages'); ?>
+<main>
+    
+    <?php get_template_part( 'template-parts/flexible-content' ) ?>
+
+</main>
+
+<?php get_footer() ?>
