4306a2ba7da16b1231954db0e2ab2d2a9268cb41
brunarafaela <brunarafaelav@outlook.com>
2021-11-04 20:38:28 -0300

    Separação do banner home

--- front-page.php
+++ front-page.php
@@ -1,32 +1,7 @@
 <?php get_header(); ?>
 <?php get_template_part('template-parts/header-home'); ?>
 <main id="ica-content">
-  <div class="ica-com com-carousel com-home swiper-container">
-    <div class="swiper-wrapper">
-      <div class="com-item swiper-slide">
-        <div class="item-media swiper-slide-img-home" style="background-image: url('<?php echo get_template_directory_uri() . '/images/header.png' ?>');background-position: center;background-repeat: no-repeat; background-size: cover;">
-        </div>
-        <div class="item-media swiper-slide-img-home-mobile" style="background-image: url('<?php echo get_template_directory_uri() . '/images/header-mobile.png' ?>');background-position: center;background-repeat: no-repeat; background-size: cover;">
-        </div>
-        <div class="item-content ica-the the-highlight">
-          <h1 class="item-title"><span>Consultoria</span> especializada em estratégia e inovação do <span>meio ambiente</span></h1>
-          <a href="#" class="ica-but but-light">Sobre a iCare</a>
-          <a href="#" class="ica-but but-light">Áreas de Atuação</a>
-        </div>
-      </div>
-      <div class="com-item swiper-slide">
-        <div class="item-media swiper-slide-img-home" style="background-image: url('<?php echo get_template_directory_uri() . '/images/header.png' ?>');background-position: center;background-repeat: no-repeat; background-size: cover;">
-        </div>
-        <div class="item-media swiper-slide-img-home-mobile" style="background-image: url('<?php echo get_template_directory_uri() . '/images/header-mobile.png' ?>');background-position: center;background-repeat: no-repeat; background-size: cover;">
-        </div>
-        <div class="item-content ica-the the-highlight">
-          <h1 class="item-title"><span>Assessoria</span> especializada em estratégia e inovação da <span>Natureza</span></h1>
-          <a href="#" class="ica-but but-light">Nossos Produtos</a>
-          <a href="#" class="ica-but but-light">Entre em Contato</a>
-        </div>
-      </div>
-    </div>
-  </div>
+<?php get_template_part('template-parts/carousel-home'); ?>
   <section class="ica-sec sec-acting" id="acting">
     <div class="ica-wrapper">
       <div class="sec-content">
