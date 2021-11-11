eed4cbff4b92dbce96896c01804ac622234544f0
Becky Marques <becky.marques@colmeiaperformance.com.br>
2021-11-11 20:19:35 -0300

    Inserção ACF no banner home / seção notícias home no template part adequado

--- template-parts/carousel-home.php
+++ template-parts/carousel-home.php
@@ -1,26 +1,35 @@
- <div class="ica-com com-carousel com-home swiper-container">
+<div class="ica-com com-carousel com-home swiper-container">
     <div class="swiper-wrapper">
-      <div class="com-item swiper-slide">
-        <div class="item-media swiper-slide-img-home" style="background-image: url('<?php echo get_template_directory_uri() . '/images/header.png' ?>');background-position: center;background-repeat: no-repeat; background-size: cover;">
+
+        <?php
+        // $banner_home = get_field('banner_home');
+        // if (is_array($banner_home) || is_object($banner_home)) {
+        // foreach ($banner_home as $bh) { 
+          
+          if( have_rows('banner_home') ):
+            while( have_rows('banner_home') ): the_row();
+          ?>
+
+        <div class="com-item swiper-slide">
+            <div class="item-media swiper-slide-img-home"
+                style="background-image: url('<?php the_sub_field('imagem_desktop_bh'); ?>');background-position: center;background-repeat: no-repeat; background-size: cover;">
+            </div>
+            <div class="item-media swiper-slide-img-home-mobile"
+                style="background-image: url('<?php the_sub_field('imagem_mobile_bh'); ?>');background-position: center;background-repeat: no-repeat; background-size: cover;">
+            </div>
+            <div class="item-content ica-the the-highlight">
+                <?php the_sub_field('titulo_bh'); 
+                ?>
+                <?php 
+                if( have_rows('botoes_bh') ):
+                while( have_rows('botoes_bh') ): the_row(); ?>
+                <a href="<?php the_sub_field('url_bbh'); ?>" class="ica-but but-light"><?php the_sub_field('rotulo_bbh'); ?></a>
+                <?php endwhile; ?>
+                <?php endif; ?>
+            </div>
         </div>
-        <div class="item-media swiper-slide-img-home-mobile" style="background-image: url('<?php echo get_template_directory_uri() . '/images/header-mobile.jpg' ?>');background-position: center;background-repeat: no-repeat; background-size: cover;">
-        </div>
-        <div class="item-content ica-the the-highlight">
-          <h1 class="item-title"><span>Consultoria</span> especializada em estratégia e inovação do <span>meio ambiente</span></h1>
-          <a href="#" class="ica-but but-light">Sobre a I Care</a>
-          <a href="#" class="ica-but but-light">Áreas de Atuação</a>
-        </div>
-      </div>
-      <div class="com-item swiper-slide">
-        <div class="item-media swiper-slide-img-home" style="background-image: url('<?php echo get_template_directory_uri() . '/images/header.png' ?>');background-position: center;background-repeat: no-repeat; background-size: cover;">
-        </div>
-        <div class="item-media swiper-slide-img-home-mobile" style="background-image: url('<?php echo get_template_directory_uri() . '/images/header-mobile.jpg' ?>');background-position: center;background-repeat: no-repeat; background-size: cover;">
-        </div>
-        <div class="item-content ica-the the-highlight">
-          <h1 class="item-title"><span>Assessoria</span> especializada em estratégia e inovação da <span>Natureza</span></h1>
-          <a href="#" class="ica-but but-light">Nossos Produtos</a>
-          <a href="#" class="ica-but but-light">Entre em Contato</a>
-        </div>
-      </div>
+
+        <?php endwhile; 
+        endif; ?>
     </div>
-  </div>
+</div>
--- template-parts/section-noticias.php
+++ template-parts/section-noticias.php
@@ -1,16 +1,24 @@
-<?php $query = new WP_Query( 'posts_per_page=3' ); ?>
-<?php while($query->have_posts()) : $query->the_post(); ?>
-<div class="item-content">
-  <div class="item-media">
-    <a href="<?php the_permalink() ?>" title="<?php the_title(); ?>" rel="bookmark">
-      <?php the_post_thumbnail(); ?>
-      <h3 class="media-title">
-      <?php if (strlen($post->post_title) > 75) {echo substr(the_title($before = '', $after = '', FALSE), 0, 75) . '...'; } else {the_title();} ?>
-      </h3>
-    </a>
-  </div>
-  <div class="media-description">
-    <p><?php the_excerpt(); ?></p>
-  </div>
-</div>
-<?php endwhile; wp_reset_postdata();?>
+<section class="ica-sec sec-notice">
+    <div class="ica-wrapper">
+        <div class="sec-itens">
+            <?php $query = new WP_Query( 'posts_per_page=3' ); ?>
+            <?php while($query->have_posts()) : $query->the_post(); ?>
+
+            <div class="item-content">
+                <div class="item-media">
+                    <a href="<?php the_permalink() ?>" title="<?php the_title(); ?>" rel="bookmark">
+                        <?php the_post_thumbnail(); ?>
+                        <h3 class="media-title">
+                            <?php if (strlen($post->post_title) > 75) {echo substr(the_title($before = '', $after = '', FALSE), 0, 75) . '...'; } else {the_title();} ?>
+                        </h3>
+                    </a>
+                </div>
+                <div class="media-description">
+                    <p><?php the_excerpt(); ?></p>
+                </div>
+            </div>
+
+            <?php endwhile; wp_reset_postdata();?>
+        </div>
+    </div>
+</section>
